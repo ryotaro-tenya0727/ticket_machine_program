88f0b0e7589f63585d4f0b0d7b00ce08a8914156
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-10-22 00:45:40 +0900

    Fix: テストコードをリファクタリング

--- ticket_machine/test/gate_test.rb
+++ ticket_machine/test/gate_test.rb
@@ -22,4 +22,19 @@ class GateTest < MiniTest::Test
     @umeda.enter(ticket: ticket)
     refute @mikuni.exit(ticket: ticket)
   end
+
+  def test_umeda_to_mikuni_when_fare_is_enough
+    ticket = Ticket.new(fare: 190)
+    # ticketにインスタンス変数代入
+    @umeda.enter(ticket: ticket)
+    assert @mikuni.exit(ticket: ticket)
+  end
+
+
+  def test_juso_to_mikuni
+    ticket = Ticket.new(fare: 150)
+    # ticketにインスタンス変数代入
+    @juso.enter(ticket: ticket)
+    assert @mikuni.exit(ticket: ticket)
+  end
 end
